d00aef4f4d2f524d544b56c969c9b67a430f3b67
davi <davijluna@gmail.com>
2022-06-08 14:43:58 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Table from './Componente/Table';
 
 function App() {
   return (
-    <span>Hello, App! </span>
+    <div>
+      <Table />
+    </div>
   );
 }
 
